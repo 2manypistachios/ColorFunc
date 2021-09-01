92c7cef79b6a4afca920df6900577086e9ade9d1
podolsk_maxi <2manypistachios@gmail.com>
2021-09-01 00:03:47 -0400

    Lint Fixes

--- pages/api/snippets/delete.js
+++ pages/api/snippets/delete.js
@@ -1,9 +1,9 @@
-import { deleteSnippet, getSnippetById } from '@/utils/Fauna';
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { deleteSnippet, getSnippetById } from '@/utils/Fauna';
 
-export default withApiAuthRequired(async function handler(req, res) {
+export default withApiAuthRequired(async (req, res) => {
   const session = getSession(req, res);
-  const nickname = session.user.nickname;
+  const {nickname} = session.user;
 
   if (req.method !== 'DELETE') {
     return res.status(405).json({ msg: 'Method not allowed' });
@@ -20,7 +20,7 @@ export default withApiAuthRequired(async function handler(req, res) {
     const deleted = await deleteSnippet(id);
     return res.status(200).json(deleted);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Something went wrong.' });
+    // console.error(err);
+    return res.status(500).json({ msg: 'Something went wrong.' });
   }
 })
