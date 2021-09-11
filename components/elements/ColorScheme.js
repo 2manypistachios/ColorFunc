068c75783e6ed50ef7d6c0be9fad4f2e3d7881b4
podolsk_maxi <2manypistachios@gmail.com>
2021-09-11 17:45:47 -0400

    ColorScheme animation

--- components/elements/ColorScheme.js
+++ components/elements/ColorScheme.js
@@ -9,11 +9,15 @@ import useStore from "@/store/useStore"
 import { hueShifts } from "@/utils/harmonies"
 import { useEffect } from "react"
 
+import { motion, useAnimation } from 'framer-motion'
+
 
 export default function ColorTypes() {
   const [harmony, setHarmony] = useStore('harmony, setHarmony')
   const options = Object.keys(hueShifts)
-  const readableOptions = options.map((shift) => shift.replace('-', ' ').replace(/^\w/g, c => c.toUpperCase()))
+
+  // See https://stackoverflow.com/questions/6251463/regex-capitalize-first-letter-every-word-also-after-a-special-character-like-a
+  const readableOptions = options.map((shift) => shift.replace(/-/g, ' ').replace(/(\b[a-z](?!\s))/g, c => c.toUpperCase()))
 
   const { getRootProps, getRadioProps } = useRadioGroup({
     name: "colorScheme",
@@ -39,53 +43,57 @@ export default function ColorTypes() {
   )
 }
 
+
 export const RadioCard = ({ children, ...props }) => {
   const hues = useStore('hues')
-  const { getInputProps, getCheckboxProps } = useRadio(props)
+  const { state, getInputProps, getCheckboxProps } = useRadio(props)
+  const { isChecked } = state;
 
   const input = getInputProps()
   const checkbox = getCheckboxProps()
 
-  let bg = "transparent"
-  const isChecked = checkbox['data-checked']
-  if (isChecked !== undefined) {
-    let hexes = hues.map((hue) => hue.toHex())
-    if (hexes.length == 1) {
-      bg = [...hexes, ...hexes]
-    } else {
-      bg = hexes
-    }
-  }
+  const controls = useAnimation()
 
-  // Todo: Clean up reactivity
   useEffect(() => {
-  }, [isChecked])
+    if (isChecked) {
+      let hexes = hues.map((hue) => hue.toHex())
+      if (hexes.length == 1) {
+        hexes = [...hexes, ...hexes]
+      }
+
+      controls.start({
+        borderImage: `linear-gradient(to right, ${hexes}) 1`,
+        borderWidth: '2px',
+      })
+    } else {
+      controls.start({
+        borderImage: '',
+        borderColor: 'var(--chakra-colors-whiteAlpha-300)',
+        borderWidth: '1px',
+      })
+    }
+  }, [isChecked, controls, hues])
+
 
   return (
-    <Box as="label" width="100%"
-
-    >
+    <Box as="label" width="100%">
       <input {...input} />
-      <Box
+      <MotionBox
         {...checkbox}
-
         cursor="pointer"
-        borderWidth="1px"
         borderRadius="md"
         boxShadow="md"
-
-        sx={{
-          borderImage: `linear-gradient(to right, ${bg}) 1`
-        }}
-
-        _checked={{
-          borderWidth: '5px',
-        }}
         px={5}
         py={3}
+
+        whileHover={{ scale: 1.1 }}
+        animate={controls}
+        transition={{ type: 'spring', bounce: 0, duration: .2}}
       >
         {children}
-      </Box>
+      </MotionBox>
     </Box>
   )
 }
+
+const MotionBox = motion(Box);
