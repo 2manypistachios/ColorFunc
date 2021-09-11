8d37603e6519c96a75d5797665fcefd864d1df80
podolsk_maxi <2manypistachios@gmail.com>
2021-09-11 02:19:14 -0400

    Fixed Breaking Snippets

--- components/elements/Snippet.js
+++ components/elements/Snippet.js
@@ -1,22 +1,21 @@
 import { Box, Heading } from "@chakra-ui/react";
-import { TwitterPicker } from "react-color";
+// import { colord } from 'colord';
 
 import useStore from "@/store/useStore"
-import { colorGen } from "@/utils/colors";
-
+// import { colorGen } from "@/utils/colors";
 import DeleteSnippet from "@/elements/DeleteSnippet"
 
 
 export default function SnippetList({ snippet, snippetDeleted }) {
-  const [setHex, setScheme, setLoop, setBrightFunc, setSatFunc] = useStore('setHex, setScheme, setLoop, setBrightFunc, setSatFunc')
-  const { harmony, startingColor, brightFunc, satFunc, loop, name } = snippet.data;
+  const [setHex, setHarmony, setLoop, setBrightFunc, setSatFunc] = useStore('setHex, setHarmony, setLoop, setBrightFunc, setSatFunc')
+  const { harmony, hex, brightFunc, satFunc, loop, name } = snippet.data;
 
-  const colorsObj = colorGen({ harmony, startingColor, brightFunc, satFunc, loop })
-  const colors = Object.values(colorsObj).flatMap((colorGroup) => [...colorGroup].flatMap(([, val]) => val.hex))
+  // const colorsObj = colorGen({ harmony, startingColor: colord(hex), brightFunc, satFunc, loop })
+  // const colors = Object.values(colorsObj).flatMap((colorGroup) => [...colorGroup].flatMap(([, val]) => val.hex))
 
   const changeAlgo = () => {
-    setHex(startingColor);
-    setScheme(harmony)
+    setHex(hex);
+    setHarmony(harmony)
     setLoop(loop)
     setBrightFunc(brightFunc)
     setSatFunc(satFunc)
@@ -27,7 +26,7 @@ export default function SnippetList({ snippet, snippetDeleted }) {
       <Heading onClick={changeAlgo} size="large" mt="1.5rem" w="80%" display="inline-block">{name}</Heading>
       <DeleteSnippet id={snippet.id} snippetDeleted={snippetDeleted} w="5%" display="inline-block"/>
       <Box onClick={changeAlgo}>
-        <TwitterPicker colors={colors} triangle="hide" color={colors[0]} />
+        {/*<TwitterPicker colors={colors} triangle="hide" color={colors[0]} />*/}
       </Box>
     </Box>
   )
