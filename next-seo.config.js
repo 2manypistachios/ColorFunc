0286fc9894b8f8ec5743e84c290bb2a2db15acb0
podolsk_maxi <2manypistachios@gmail.com>
2021-09-17 17:45:10 -0400

    Changed OG image

--- next-seo.config.js
+++ next-seo.config.js
@@ -13,10 +13,10 @@ const SEO = {
     description,
     images: [
       {
-        url: `https://og-image.vercel.app/**Next.js%20Chakra**%20Starter.png?theme=light&md=1&fontSize=125px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fraw.githubusercontent.com%2Fchakra-ui%2Fchakra-ui%2Fbf775929a6d73a3aa69e44d5d38542449871475c%2Flogo%2Flogomark-colored.svg`,
+        url: `colorFuncOG.png`,
         alt: title,
-        width: 2048,
-        height: 1170,
+        width: 1199,
+        height: 693,
       },
     ],
   },
